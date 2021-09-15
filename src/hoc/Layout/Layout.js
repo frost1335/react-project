f018943cc339eaf0306b8e5189662d9400cabaaf
raximvodil <65526165+raximvodil@users.noreply.github.com>
2021-09-15 13:03:26 +0500

    react app start !

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <main>{this.props.children}</main>
+      </div>
+    );
+  }
+}
+
+export default Layout;
